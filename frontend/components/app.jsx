a276d8d6f97cf89c71e32cfdd3e39ad3c0cb8ab7
App Academy Student <student@appacademy.io>
2016-04-06 14:21:09 -0700

    npm set up

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div id="content">
+        hello
+      </div>
+    );
+  }
+
+});
+
+module.exports =  App;
